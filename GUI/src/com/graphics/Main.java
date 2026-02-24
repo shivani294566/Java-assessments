d5b8e0ddf61c310a329ea0936b7471af8d83120b
shivani294566 <159241960+shivani294566@users.noreply.github.com>
2026-02-24 23:25:13 +0530

    Add files via upload

---
+++ GUI/src/com/graphics/Main.java
@@ -0,0 +1,10 @@
+package com.graphics;
+import javax.swing.JOptionPane;
+public class Main {
+
+	public static void main(String[] args) {
+       String name=JOptionPane.showInputDialog("Enter your name");
+       JOptionPane.showMessageDialog(null,"Hello"+name);
+	}
+
+}
